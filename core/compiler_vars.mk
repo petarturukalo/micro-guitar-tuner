3594610f081f9c8cb9957bdc4956a84016d472b7
Petar Turukalo <petarturukalo@gmail.com>
2024-05-25 15:29:09 +1000

    resolve compiler_vars.mk TODOs

--- core/compiler_vars.mk
+++ core/compiler_vars.mk
@@ -3,7 +3,7 @@
 #
 # This makefile asserts the following variables are set.
 # - arm_arch_profile: A for Cortex-A or M for Cortex-M
-# - cross_prefix: cross compilation prefix. Required for Cortex-M. (TODO)
+# - cross_prefix: cross compilation prefix. Required for Cortex-M. 
 # - cpu: processor to target. Only for Cortex-M, and required for it.
 
 arm_arch_profile_error_msg = "Variable arm_arch_profile not defined or set correctly: set it to either A for Cortex-A or M for Cortex-M"
@@ -17,7 +17,7 @@ endif
 endif
 
 ifeq ($(arm_arch_profile), M)
-# TODO test on rpi4b
+# TODO test compiling natively (not cross compiling) on rpi4b
 ifndef cross_prefix
 $(error Cortex-M needs variable cross-prefix defined)
 endif
@@ -36,5 +36,3 @@ else
 CFLAGS += -mcpu=$(cpu)
 endif
 
-# TODO 
-# - test compiling natively (not cross compiling) on rpi4
